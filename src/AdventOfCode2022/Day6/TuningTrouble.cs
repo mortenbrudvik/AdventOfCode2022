75a7ed40e77fa779dd62c8e741b0832a3bf9e0d1
Morten Brudvik <morten@brudvik.com>
2022-12-06 10:47:20 +0100

    chore: Day6, Part 1 prepping

---
+++ src/AdventOfCode2022/Day6/TuningTrouble.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2022.Day6;
+
+public class TuningTrouble
+{
+    private readonly ITestOutputHelper _logger;
+
+    public TuningTrouble(ITestOutputHelper logger)
+    {
+        _logger = logger;
+    }
+    
+    [Fact]
+    public void Part1_FindHowManyCharactersNeedToBeProcessed()
+    {
+        
+    }
+}
