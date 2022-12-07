44eb00fdc40e5cc72f7c5c541d7be465631df58b
Morten Brudvik <morten@brudvik.com>
2022-12-07 09:51:56 +0100

    refactor: Improved code for day 6

--- src/AdventOfCode2022/Day6/TuningTrouble.cs
+++ src/AdventOfCode2022/Day6/TuningTrouble.cs
@@ -10,32 +10,21 @@ namespace AdventOfCode2022.Day6;
 public class TuningTrouble
 {
     private readonly ITestOutputHelper _logger;
-
     public TuningTrouble(ITestOutputHelper logger) => _logger = logger;
 
     [Fact]
-    public void Part1_FindPacketMarker()
-    {
-        var markedPosition = File.ReadAllText("Day6/input.txt")
-            .Scan("", (a, b) => a + b)
-            .TakeUntil(x => 
-                x.TakeLast(4).Length() == 4 &&
-                x.TakeLast(4).Distinct().Length() == x.TakeLast(4).Length())
-            .Last().Length;
-        
-        _logger.WriteLine("Start of packet marker received at position: " + markedPosition);
-    }
+    public void Part1_FindPacketMarker() => 
+        _logger.WriteLine("Start of packet marker received at position: " + FindPacketMarkerPosition("Day6/input.txt"));
 
     [Fact]
-    public void Part2_FindMessageMarker()
-    {
-        var markedPosition = File.ReadAllText("Day6/input.txt")
+    public void Part2_FindMessageMarker() => 
+        _logger.WriteLine("Start of message marker received at position: " + FindMessageMarkerPosition("Day6/input.txt"));
+
+    private static int FindPacketMarkerPosition(string filePath) => FindMarkerPosition(filePath, 4); 
+    private static int FindMessageMarkerPosition(string filePath) => FindMarkerPosition(filePath, 14);
+    
+    private static int FindMarkerPosition(string filePath, int length) =>
+        File.ReadAllText(filePath)
             .Scan("", (a, b) => a + b)
-            .TakeUntil(x => 
-                x.TakeLast(14).Length() == 14 &&
-                x.TakeLast(14).Distinct().Length() == x.TakeLast(14).Length())
-            .Last().Length;
-        
-        _logger.WriteLine("Start of message marker received at position: " + markedPosition);
-    }
-}
+            .TakeUntil(x => x.Length >= length && x[^length..].Distinct().Length() == x[^length..].Length)
+            .Last().Length;}
