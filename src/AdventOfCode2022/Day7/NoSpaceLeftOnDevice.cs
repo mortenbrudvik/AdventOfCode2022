c0c8245fe3c3a10fc55c3d246b5679be12758158
Morten Brudvik <morten@brudvik.com>
2022-12-07 10:01:58 +0100

    chore: Day7, prepping

---
+++ src/AdventOfCode2022/Day7/NoSpaceLeftOnDevice.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2022.Day7;
+
+public class NoSpaceLeftOnDevice
+{
+    private readonly ITestOutputHelper _logger;
+    public NoSpaceLeftOnDevice(ITestOutputHelper logger) => _logger = logger;
+
+    [Fact]
+    public void Part_()
+    {
+        
+    }
+}
