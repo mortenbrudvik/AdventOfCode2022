9c233ef6d984383947b54c8eb93761d45fa61e9d
Morten Brudvik <morten@brudvik.com>
2022-12-08 23:04:48 +0100

    chore: Day7, Part 1 - traverse directories and calculate file sizes

--- src/AdventOfCode2022/Day7/NoSpaceLeftOnDevice.cs
+++ src/AdventOfCode2022/Day7/NoSpaceLeftOnDevice.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace AdventOfCode2022.Day7;
@@ -7,10 +10,38 @@ public class NoSpaceLeftOnDevice
 {
     private readonly ITestOutputHelper _logger;
     public NoSpaceLeftOnDevice(ITestOutputHelper logger) => _logger = logger;
-
+    
     [Fact]
     public void Part_()
     {
+        var lines = File.ReadLines("Day7/input.txt").Skip(1);
+        var currentDir = "/";
+        var sizes = new Dictionary<string, long> {{currentDir, 0}};
+
+        foreach (var line in lines.Select(x => x.Split(' ')))
+        {
+            if (line[0] =="$")
+            {
+                if(line[1] == "ls") 
+                    continue;
+                
+                if (line[1] == "cd" && line[2] == "..")
+                {
+                    _logger.WriteLine("" + currentDir);
+                    currentDir = currentDir.Trim('/').Split('/').SkipLast().Aggregate("/", (a, b) => a + b + "/");
+                } 
+                
+                if (line[1] == "cd" && line[2].All(char.IsLetter)) 
+                    currentDir += line[2] + "/"; 
+            }
+            
+            if(line[0] == "dir") sizes.Add(currentDir + line[1] + "/", 0);
+            if (line[0].All(char.IsDigit)) sizes[currentDir] += int.Parse(line[0]);
+
+        }
         
+        _logger.WriteLine("Sum of file space: " + sizes.Values.Sum());
     }
 }
+
+
