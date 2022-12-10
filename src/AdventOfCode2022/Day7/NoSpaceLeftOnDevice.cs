fc72a5a7c68c9c2ad1a75b1ea5cdba31165dfc55
Morten Brudvik <morten@brudvik.com>
2022-12-10 11:44:51 +0100

    chore: Day 7, part 1 completed

--- src/AdventOfCode2022/Day7/NoSpaceLeftOnDevice.cs
+++ src/AdventOfCode2022/Day7/NoSpaceLeftOnDevice.cs
@@ -1,47 +1,74 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;  
+using System.IO;  
 using System.Linq;
-using Xunit;
-using Xunit.Abstractions;
-
+using FluentAssertions;
+using Xunit;  
+using Xunit.Abstractions;  
+  
 namespace AdventOfCode2022.Day7;
 
 public class NoSpaceLeftOnDevice
 {
     private readonly ITestOutputHelper _logger;
     public NoSpaceLeftOnDevice(ITestOutputHelper logger) => _logger = logger;
-    
+
     [Fact]
-    public void Part_()
+    public void Part1_CalculateSumOfFoldersBelowAGivenSize()
     {
         var lines = File.ReadLines("Day7/input.txt").Skip(1);
-        var currentDir = "/";
-        var sizes = new Dictionary<string, long> {{currentDir, 0}};
+        var rootFolder = new Folder("/", null);
+        var currentFolder = rootFolder;
 
         foreach (var line in lines.Select(x => x.Split(' ')))
         {
-            if (line[0] =="$")
+            if (line[0] == "$")
             {
-                if(line[1] == "ls") 
-                    continue;
-                
-                if (line[1] == "cd" && line[2] == "..")
+                currentFolder = line[1] switch
                 {
-                    _logger.WriteLine("" + currentDir);
-                    currentDir = currentDir.Trim('/').Split('/').SkipLast().Aggregate("/", (a, b) => a + b + "/");
-                } 
-                
-                if (line[1] == "cd" && line[2].All(char.IsLetter)) 
-                    currentDir += line[2] + "/"; 
+                    "cd" when line[2] == ".." => currentFolder.Parent!,
+                    "cd" when line[2].All(char.IsLetter) => currentFolder.Children.Single(x => x.Name == line[2]),
+                    _ => currentFolder
+                };
             }
-            
-            if(line[0] == "dir") sizes.Add(currentDir + line[1] + "/", 0);
-            if (line[0].All(char.IsDigit)) sizes[currentDir] += int.Parse(line[0]);
 
+            if (line[0] == "dir")
+                currentFolder.Children.Add(new Folder(line[1], currentFolder));
+            else if (line[0].All(char.IsDigit))
+                currentFolder.Size += long.Parse(line[0]);
         }
+
+        var total = SetTotalFolderSize(rootFolder);
+        var totalWithSizeBelow = CalculateFolders(rootFolder, 100000);
         
-        _logger.WriteLine("Sum of file space: " + sizes.Values.Sum());
+        _logger.WriteLine("Total size of all folders: " + total);
+        _logger.WriteLine("Total size of all folders below : 100000 bytes: " + totalWithSizeBelow);
+
+        totalWithSizeBelow.Should().Be(1325919);
+    }
+
+    private static long SetTotalFolderSize(Folder folder)
+    {
+        folder.Size += folder.Children.Sum(SetTotalFolderSize);
+        return folder.Size;
+    }
+
+    private static long CalculateFolders(Folder folder, long withSizeBelow)
+    {
+        var size = folder.Children.Sum(x => CalculateFolders(x, withSizeBelow));
+        return folder.Size <= withSizeBelow ? folder.Size + size : size;
+    }
+
+    public class Folder
+    {
+        public Folder(string name, Folder? parent)
+        {
+            Name = name;
+            Parent = parent;
+        }
+
+        public string Name { get; }
+        public List<Folder> Children { get; } = new();
+        public Folder? Parent { get; }
+        public long Size { get; set; }
     }
 }
-
-
