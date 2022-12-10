c68b7b8eb9ce814d14912589cad307583cc79f2d
Morten Brudvik <morten@brudvik.com>
2022-12-10 15:23:13 +0100

    chore: Day 10, Part 1 Prepping

---
+++ src/AdventOfCode2022/Day10/CathodeRayTube.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2022.Day10;
+
+public class CathodeRayTube
+{
+    private readonly ITestOutputHelper _logger;
+
+    public CathodeRayTube(ITestOutputHelper logger)
+    {
+        _logger = logger;
+    }
+
+    [Fact]
+    public void Part1()
+    {
+        
+    }
+}
