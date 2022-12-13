3c7858cadf7015f6a653d5182ed46d01c954d1ab
Morten Brudvik <morten@brudvik.com>
2022-12-13 08:01:57 +0100

    chore: Day 13, prepping

---
+++ src/AdventOfCode2022/Day13/DistressSignal.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode2022.Day13;
+
+public class DistressSignal : XunitContextBase
+{
+    [Fact]
+    public void MethodName()
+    {
+        
+    }
+
+    public DistressSignal(ITestOutputHelper logger) : base(logger)
+    {
+    }
+}
--- src/AdventOfCode2022/LoggerExtensions.cs
+++ src/AdventOfCode2022/LoggerExtensions.cs
@@ -1,11 +1,10 @@
 using System.Collections.Generic;
-using Xunit.Abstractions;
+using Xunit;
 
 namespace AdventOfCode2022;
 
 public static class LoggerExtensions
 {
-    public static void LogResult<T>(this IEnumerable<T> data, ITestOutputHelper logger) =>
-        logger.WriteLine("" + string.Join(',', data));
-
+    public static void LogResult<T>(this IEnumerable<T> data) =>
+        XunitContext.WriteLine("" + string.Join(',', data));
 }
